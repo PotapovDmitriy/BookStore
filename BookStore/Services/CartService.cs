20bc667abf26df5d23406d589f0e3c6f51e2aa8e
Potapov Dmitriy <PotapovDI1@outlook.com>
2021-11-11 13:26:42 +0500

    Начал реализацию метода для подсчета финальной стоимости корзины

--- BookStore/Services/CartService.cs
+++ BookStore/Services/CartService.cs
@@ -1,7 +1,51 @@
-﻿namespace BookStore.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookStore.Enums;
+using BookStore.Interfaces;
+using BookStore.Models;
+
+namespace BookStore.Services
 {
     public class CartService
     {
-        //deliverPrice
+        //deliverPrice = 200
+
+        public double GetFinalPrice(Cart cart, double deliverPrice = 200)
+        {
+            var totalSum = cart.Books.Select(i => i.Price).Sum();
+
+            return 0;
+        }
+
+        private double GetStockSale(List<IBook> books, Stock stock)
+        {
+            double result = 0;
+            switch (stock.Type)
+            {
+                case StockType.FreeEBook:
+                {
+                    var freeEBook = CheckFreeEBook(books);
+                    result = freeEBook?.Price ?? 0;
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return result;
+        }
+
+        private IBook CheckFreeEBook(List<IBook> books)
+        {
+            var paperBook = books.Where(i => i is PaperBook).ToList();
+            var targetAuthor = paperBook.GroupBy(i => i.Author).Select(group => new
+            {
+                Author = group.Key,
+                Count = group.Count()
+            });
+            
+
+        }
     }
 }
