01b31e29855f65b64f9300a443f257752105401e
potapov <lhfujy7477@gmail.com>
2021-11-23 18:58:18 +0500

    Поправил подсчет скидки

--- BookStore/Models/PromoCodes/SaleCurrencyPromoCode.cs
+++ BookStore/Models/PromoCodes/SaleCurrencyPromoCode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BookStore.Interfaces;
 
 namespace BookStore.Models.PromoCodes
@@ -10,6 +11,6 @@ namespace BookStore.Models.PromoCodes
         public double Value { get; set; }
 
         public double GetSaleSum(IPromoCodeItem item)
-            => Value;
+            => item.TotalSum is null ? 0 : Math.Min((double)item.TotalSum, Value);
     }
 }
