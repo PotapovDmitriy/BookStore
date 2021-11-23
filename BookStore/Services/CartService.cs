1c7089003604b9d581c41fd30a1d063e56d553cf
potapov <lhfujy7477@gmail.com>
2021-11-23 19:02:22 +0500

    Поправил подсчет финалльной цены

--- BookStore/Services/CartService.cs
+++ BookStore/Services/CartService.cs
@@ -12,10 +12,10 @@ namespace BookStore.Services
         {
             var totalSum = cart.Books.Select(i => i.Price).Sum();
             var stockSale = cart.Promo.GetSaleSum(cart.Books);
-            var finalDeliverPrice = cart.Deliver.GetDeliverPrice(cart.Books);
+            var finalDeliverPrice = cart.Deliver?.GetDeliverPrice(cart.Books) ?? 0;
             var intermediatePrice = totalSum + finalDeliverPrice;
             var promoCodeItem = new PromoCodeItem(cart.Books, intermediatePrice, cart.Deliver.Value);
-            var promoCodeSale = cart.PromoCode.GetSaleSum(promoCodeItem);
+            var promoCodeSale = cart.PromoCode?.GetSaleSum(promoCodeItem) ?? 0;
             var resultSale = promoCodeSale > stockSale ? promoCodeSale : stockSale;
 
             return intermediatePrice - resultSale;
